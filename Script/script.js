fa6e239e8980881fd6407cccca6266b52a83ea06
SimonBelin <belin.simon1995@gmail.com>
2021-02-19 09:09:51 +0100

    construction site

--- Script/script.js
+++ Script/script.js
@@ -2,6 +2,17 @@ $("#button").click(function() {
     $('.transform').toggleClass('transform-active');
   });
 
+$(document).ready(function(){
+  $('.burger').click(function(e){
+    e.preventDefault();
+    $this = $(this);
+    if($this.hasClass('is-opened')){
+      $this.addClass('is-closed').removeClass('is-opened');
+    }else{
+      $this.removeClass('is-closed').addClass('is-opened');
+    }
+  })
+});
 
 
 //  $("li").click(function() {
